23d7acda3024b7c0493dfaa8470fb271d0c3b5c0
damini-k <84567449+damini-k@users.noreply.github.com>
2021-05-24 09:17:57 -0700

    Create data of images.py

---
+++ data of images.py
@@ -0,0 +1,3 @@
+downloader.download("India",limit=30,output_dir='images',adult_filter_off=True)
+downloader.download("germany",limit=30,output_dir='images',adult_filter_off=True)
+downloader.download("greece",limit=30,output_dir='images',adult_filter_off=True)
